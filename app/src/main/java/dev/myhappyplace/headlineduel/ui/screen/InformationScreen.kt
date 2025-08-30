3480ee56f906294e30590a199afb487efee54c2b
GLS <gemmalarasav@gmail.com>
2025-08-30 11:44:37 +0200

    Add Information Screen  text

--- app/src/main/java/dev/myhappyplace/headlineduel/ui/screen/InformationScreen.kt
+++ app/src/main/java/dev/myhappyplace/headlineduel/ui/screen/InformationScreen.kt
@@ -1,8 +1,10 @@
 package dev.myhappyplace.headlineduel.ui.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -17,8 +19,11 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.myhappyplace.headlineduel.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -53,8 +58,27 @@ fun InformationScreen(onBack: () -> Unit) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
+                .padding(16.dp)
         ) {
-            Text(text = "This is the information screen.")
+            Text(
+                text = stringResource(id = R.string.information_title),
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = stringResource(id = R.string.information_body),
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontSize = 18.sp
+                ),
+            )
         }
     }
 }
+
+@Preview
+@Composable
+private fun InformationScreenPreview() {
+    InformationScreen(onBack = {})
+}
+
