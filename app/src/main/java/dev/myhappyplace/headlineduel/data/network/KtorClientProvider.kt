12591f0b66627562cbd6ac91dfeec2c234d96508
GLS <gemmalarasav@gmail.com>
2025-11-24 15:24:08 +0100

    Fix: Handle network errors. Gives Hugging Face Space enough time to spin up from a cold start without causing an immediate timeout error.

--- app/src/main/java/dev/myhappyplace/headlineduel/data/network/KtorClientProvider.kt
+++ app/src/main/java/dev/myhappyplace/headlineduel/data/network/KtorClientProvider.kt
@@ -2,6 +2,7 @@ package dev.myhappyplace.headlineduel.data.network
 
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
@@ -13,6 +14,11 @@ object KtorClientProvider {
                 json(Json {
                     ignoreUnknownKeys = true
                 })
+            }
+            install(HttpTimeout) {
+                requestTimeoutMillis = 60000
+                connectTimeoutMillis = 60000
+                socketTimeoutMillis = 60000
             }
         }
     }
